349a0a0ec240e98ee4052106c87674d5bd7c22f6
Rajesh-107 <applerb326@gmail.com>
2022-10-31 23:57:51 +0600

    user api

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,4 +5,12 @@ const UsersController = require("../controllers/Users/UsersController")
 
 const router = express.Router()
 
+//user Profile
+router.post("/Registration", UsersController.Registration)
+router.post("/Login",UsersController.Login)
+router.post("/ProfileUpdate", AuthVerifyMiddleware,UsersController.ProfileUpdate)
+router.post("/ProfileDetails",AuthVerifyMiddleware , UsersController.ProfileDetails)
+router.post("/Login",UsersController.Login)
+router.post("/Login",UsersController.Login)
+
 module.exports = router;
