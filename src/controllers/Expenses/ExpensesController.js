e1866d213aed5b504d1d50b9cd2365be1c4895b3
Rajesh-107 <applerb326@gmail.com>
2022-11-22 00:06:27 +0600

    expenses list

--- src/controllers/Expenses/ExpensesController.js
+++ src/controllers/Expenses/ExpensesController.js
@@ -1,5 +1,6 @@
 const DataModel = require("../../models/Expenses/ExpensesModel")
 const CreateService = require("../../services/common/CreateService");
+const ListOnejoinService = require("../../services/common/ListOnejoinService");
 const UpdateService = require("../../services/common/UpdateService");
 
 exports.CreateExpenses = async (req, res) => {
@@ -9,5 +10,16 @@ exports.CreateExpenses = async (req, res) => {
 
 exports.UpdateExpenses = async(req, res) => {
     let Result = await UpdateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ExpensesList = async (req, res) => {
+
+    let SearchRgx = {"regex": req.params.searchKeyword, "$options": "i"}
+    let SearchArray = [{Note: SearchRgx}, {Amount: SearchRgx}, {'Type.Name': SearchRgx}]
+    let JoinStage = {$lookup: {from: "expenseTypes", localField: "TypeID", foreignField: "_id", as: "Type"}}
+
+    let Result = await ListOnejoinService(req,DataModel, SearchArray, JoinStage)
+
     res.status(200).json(Result)
 }
